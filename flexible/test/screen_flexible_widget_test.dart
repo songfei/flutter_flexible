0b1bff5aedf24f323b3a4ab39872f5250c078c73
songfei <songfei.org@gmail.com>
2019-06-25 13:46:56 +0800

    add unit test

---
+++ flexible/test/screen_flexible_widget_test.dart
@@ -0,0 +1,68 @@
+import 'package:flexible/flexible.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('默认宽度测试', (WidgetTester tester) async {
+    double value;
+
+    await tester.pumpWidget(ScreenFlexibleWidget(
+      child: Builder(
+        builder: (BuildContext context) {
+          value = flexible(context, 375.0);
+          return Container();
+        },
+      ),
+    ));
+
+    expect(value, 375.0);
+  });
+
+  testWidgets('设置宽度测试', (WidgetTester tester) async {
+    double value;
+
+    await tester.pumpWidget(ScreenFlexibleWidget(
+      baseWidgetWidth: 750.0,
+      child: Builder(
+        builder: (BuildContext context) {
+          value = flexible(context, 375.0);
+          return Container();
+        },
+      ),
+    ));
+
+    expect(value, 750.0);
+  });
+
+  testWidgets('最大值测试', (WidgetTester tester) async {
+    double value;
+
+    await tester.pumpWidget(ScreenFlexibleWidget(
+      baseWidgetWidth: 750.0,
+      child: Builder(
+        builder: (BuildContext context) {
+          value = flexible(context, 375.0, maxValue: 200.0);
+          return Container();
+        },
+      ),
+    ));
+
+    expect(value, 200.0);
+  });
+
+  testWidgets('最小值测试', (WidgetTester tester) async {
+    double value;
+
+    await tester.pumpWidget(ScreenFlexibleWidget(
+      baseWidgetWidth: 750.0,
+      child: Builder(
+        builder: (BuildContext context) {
+          value = flexible(context, 375.0, minValue: 800.0);
+          return Container();
+        },
+      ),
+    ));
+
+    expect(value, 800.0);
+  });
+}
