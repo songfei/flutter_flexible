b81aea8978efc1127a1de262e44b6bff116bd6f0
songfei <songfei.org@gmail.com>
2019-06-25 13:16:59 +0800

    unit test

--- flexible/test/flexible_test.dart
+++ flexible/test/flexible_test.dart
@@ -2,12 +2,14 @@ import 'package:flexible/screen_flexible.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('adds one to input values', () {
+  test('默认值测试', () {
     var value = ScreenFlexible.getFinalValue(375.0, 375.0);
     expect(value, 375.0);
+  });
 
+  test('指定值测试', () {
     ScreenFlexible.designScreenWidth = 750.0;
-    var value2 = ScreenFlexible.getFinalValue(750.0, 375.0);
-    expect(value2, 375.0);
+    var value = ScreenFlexible.getFinalValue(750.0, 375.0);
+    expect(value, 375.0);
   });
 }
